2db550fbff7ade90209a4f783e9594030e0530bc
ManishRajora <vmanish2514@gmail.com>
2025-10-18 10:14:57 +0530

    complete

--- random_user_info/lib/screens/home_screen.dart
+++ random_user_info/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -25,10 +27,10 @@ class _HomeScreenState extends State<HomeScreen>{
     fetchUsers();
   }
 
-  Future<void> fetchUsers() async{
-    final res = await http.get(Uri.parse('https://randomuser.me/api/?results=20'));
-    
+  Future<void> fetchUsers() async{    
     try{
+      final res = await http.get(Uri.parse('https://randomuser.me/api/?results=20')).timeout(const Duration(seconds: 10));
+
       if(res.statusCode == 200){
         final data = json.decode(res.body);
         setState(() {
@@ -42,10 +44,20 @@ class _HomeScreenState extends State<HomeScreen>{
           errorMessage = 'Failed to load users: ${res.statusCode}';
         });
       }
+    }on TimeoutException{
+      setState(() {
+        isLoading = false;
+        errorMessage = 'Request Timeout';
+      });
+    }on SocketException{
+      setState(() {
+        isLoading = false;
+        errorMessage = 'Network Error';
+      });
     }catch(err){
       setState(() {
         isLoading = false;
-        errorMessage = 'Error: $err';
+        errorMessage = 'Something Went Wrong';
       });
     }
   }
@@ -68,85 +80,120 @@ class _HomeScreenState extends State<HomeScreen>{
       ),
       body: isLoading
             ? Center(child: CircularProgressIndicator(),)
-            : errorMessage != null
-              ? Center(child: Text(errorMessage!),)
-              : GridView.builder(
-                  padding: const EdgeInsets.only(top: 10, bottom: 10, left: 5, right: 5),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.8,
-                    crossAxisSpacing: 5,
-                    mainAxisSpacing: 5,
+            : RefreshIndicator(
+              onRefresh: fetchUsers,
+              child: errorMessage != null
+                ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(errorMessage!),
+                      SizedBox(height: 15,),
+                      ElevatedButton(
+                        onPressed: (){
+                          setState(() {
+                            isLoading = true;
+                            fetchUsers();
+                          });
+                        },
+                        child: Text('Reload'),
+                      ),
+                    ],
                   ),
-                  itemCount: users.length,
-                  itemBuilder: (context, index){
-                    final user = users[index];
-                    final uuid = user['login']['uuid'];
-                    final isFavorite = _favorites.contains(uuid);
-
-                    return InkWell(
-                      onTap: (){
-                        Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => UserDetailsScreen(user: user, id: uuid,)));
-                      },
-                      child: Card(
-                        elevation: 2,
-                        clipBehavior: Clip.hardEdge,
-                        child: Stack(
-                          children: [
-                            Hero(
-                              tag: uuid,
-                              child: FadeInImage(
-                                placeholder: AssetImage('assets/placeholder2.jpg'),
-                                image: NetworkImage(user['picture']['large']),
-                                fit: BoxFit.cover,
-                                height: 400,
-                                width: double.infinity,
-                              ),
-                            ),
-                      
-                            Positioned(
-                              left: 0,
-                              right: 0,
-                              bottom: 0,
-                              child: Container(
-                                color: Colors.black38,
-                                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 6),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text('${user['name']['first']}, ${user['dob']['age']}', style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.bold,
-                                        ),),
-                                        Text('${user['location']['country']}', style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 9,
-                                        ),),
-                                      ],
-                                    ),
-                      
-                                    IconButton(
-                                      onPressed: (){
-                                        _toggleFavorite(uuid);
-                                      },
-                                      icon: isFavorite
-                                            ? Icon(Icons.favorite, color: Colors.red,)
-                                            : Icon(Icons.favorite_border, color: Colors.white,),
-                                    ),
-                                  ],
+                )
+                : GridView.builder(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10, left: 5, right: 5),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.8,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5,
+                    ),
+                    itemCount: users.length,
+                    itemBuilder: (context, index){
+                      final user = users[index];
+                      final uuid = user['login']['uuid'];
+                      final isFavorite = _favorites.contains(uuid);
+              
+                      return InkWell(
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (ctx) => UserDetailsScreen(
+                              user: user,
+                              id: uuid,
+                              favorites: _favorites,
+                              onToggle: (id) {
+                                setState(() {
+                                  if(_favorites.contains(id)){
+                                    _favorites.remove(id);
+                                  }else{
+                                    _favorites.add(id);
+                                  }
+                                });
+                              },
+                            ))
+                          );
+                        },
+                        child: Card(
+                          elevation: 2,
+                          clipBehavior: Clip.hardEdge,
+                          child: Stack(
+                            children: [
+                              Hero(
+                                tag: uuid,
+                                child: FadeInImage(
+                                  placeholder: AssetImage('assets/placeholder2.jpg'),
+                                  image: NetworkImage(user['picture']['large']),
+                                  fit: BoxFit.cover,
+                                  height: 400,
+                                  width: double.infinity,
                                 ),
                               ),
-                            ),
-                          ],
+                        
+                              Positioned(
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                child: Container(
+                                  color: Colors.black38,
+                                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 6),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text('${user['name']['first']} ${user['dob']['age']}', style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 11,
+                                            fontWeight: FontWeight.bold,
+                                          ),),
+                                          Text('${user['location']['country']}', style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 9,
+                                          ),),
+                                        ],
+                                      ),
+                        
+                                      IconButton(
+                                        onPressed: (){
+                                          _toggleFavorite(uuid);
+                                        },
+                                        icon: isFavorite
+                                              ? Icon(Icons.favorite, color: Colors.red,)
+                                              : Icon(Icons.favorite_border, color: Colors.white,),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
+                      );
+                    },
+                  ),
+            ),
     );
   }
 }
--- random_user_info/lib/screens/user_details_screen.dart
+++ random_user_info/lib/screens/user_details_screen.dart
@@ -1,29 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:random_user_info/screens/home_screen.dart';
 
-class UserDetailsScreen extends StatelessWidget{
-  const UserDetailsScreen({super.key, required this.user, required this.id});
+class UserDetailsScreen extends StatefulWidget{
+  const UserDetailsScreen({super.key, required this.user, required this.id, required this.favorites, required this.onToggle});
 
   final Map<String, dynamic> user;
   final String id;
+  final Set<String> favorites;
+  final ValueChanged<String> onToggle;
+
+  @override
+  State<UserDetailsScreen> createState() => _UserDetailsScreenState();
+}
+
+class _UserDetailsScreenState extends State<UserDetailsScreen> {
+  late bool _isFav;
+
+  @override
+  void initState() {
+    super.initState();
+    _isFav = widget.favorites.contains(widget.id);
+  }
+
+  void _handleToggle(){
+    setState(() {
+      _isFav = !_isFav;
+    });
+    widget.onToggle(widget.id);    // telling homescreen to update favorite set
+  }
 
   @override
   Widget build(BuildContext context) {
-    final name = '${user['name']['first']} ${user['name']['last']}';
-    final age = '${user['dob']['age']}';
-    final city = '${user['location']['city']}';
-    final country = '${user['location']['country']}';
-    final image = user['picture']['large'];
+    final name = '${widget.user['name']['first']} ${widget.user['name']['last']}';
+    final age = '${widget.user['dob']['age']}';
+    final city = '${widget.user['location']['city']}';
+    final country = '${widget.user['location']['country']}';
+    final image = widget.user['picture']['large'];
 
     return Scaffold(
       appBar: AppBar(
         title: Text('User'),
+        actions: [
+          IconButton(
+            onPressed: _handleToggle,
+            icon: widget.favorites.contains(widget.id)
+                  ? Icon(Icons.favorite, color: Colors.red,)
+                  : Icon(Icons.favorite_border, color: Colors.black,),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Hero(
-              tag: id,
+              tag: widget.id,
               child: FadeInImage(
                 placeholder: AssetImage('assets/placeholder2.jpg'),
                 image: NetworkImage(image),
